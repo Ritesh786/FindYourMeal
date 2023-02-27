9a283c929398aade10c7c98ea39915e480f4c93b
Ritesh <jharitesh6@gmail.com>
2023-02-27 11:41:10 +0530

    add UseCaseTestCase and repositoryTestCase

---
+++ data/src/test/java/demo/demodata/data/TestData.kt
@@ -0,0 +1,64 @@
+package demo.demodata.data
+
+import demo.demodata.model.MealDTO
+import demo.demodomain.model.Meal
+
+
+fun getDummyMealDto() = MealDTO(
+    idMeal = "1234",
+    strMeal = "description",
+    strMealThumb = "imageUrl",
+    dateModified = "",
+    strArea = "",
+    strCategory = "",
+    strCreativeCommonsConfirmed = "",
+    strDrinkAlternate = "",
+    strImageSource = "",
+    strIngredient1= "",
+    strIngredient10 = "",
+    strIngredient11 = "",
+    strIngredient12 = "",
+    strIngredient13 = "",
+    strIngredient14 = "",
+    strIngredient15= "",
+    strIngredient16 = "",
+    strIngredient17 = "",
+    strIngredient19 = "",
+    strIngredient18 = "",
+    strIngredient2 = "",
+    strIngredient20 = "",
+    strIngredient3 = "",
+    strIngredient4 = "",
+    strIngredient5="",
+    strIngredient6="",
+    strIngredient7="",
+    strIngredient8="",
+    strIngredient9="",
+    strInstructions="",
+    strMeasure1="",
+    strMeasure10="",
+    strMeasure11="",
+    strMeasure12="",
+    strMeasure13="",
+    strMeasure14="",
+    strMeasure15="",
+    strMeasure16="",
+    strMeasure17="",
+    strMeasure18="",
+    strMeasure19="",
+    strMeasure2="",
+    strMeasure20="",
+    strMeasure3="",
+    strMeasure4="",
+    strMeasure5="",
+    strMeasure6="",
+    strMeasure7="",
+    strMeasure8="",
+    strMeasure9="",
+    strSource="",
+    strTags="",
+    strYoutube=""
+
+)
+
+
---
+++ domain/src/test/java/demo/demodomain/domain/GetMealItemListUseCaseTest.kt
@@ -0,0 +1,47 @@
+package demo.demodomain.domain
+
+import demo.demodomain.repository.FindYourMealRepository
+import demo.demodomain.use_case.GetMealItemListUseCase
+import io.mockk.mockk
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.*
+
+import demo.demodomain.model.Meal
+import io.mockk.coEvery
+import junit.framework.TestCase
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.flow.first
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class GetMealItemListUseCaseTest {
+    private val findYourMealRepository = mockk<FindYourMealRepository>()
+
+    private fun getMealItemListUseCase(): GetMealItemListUseCase = GetMealItemListUseCase(
+        findYourMealRepository
+    )
+
+    @Test
+    fun testMealItemList() = runBlocking {
+        val mealList = listOf(Meal("52920", "Chicken","https://www.themealdb.com/images/media/meals/wyxwsp1486979827.jpg"))
+        coEvery {
+            findYourMealRepository.getMealList("potato")
+        } returns mealList
+
+        var meal: Meal? = null
+        val output = getMealItemListUseCase().invoke("potato")
+        output.collect{
+            meal = it.data?.first()
+        }
+
+        assertEquals(
+            mealList[0],
+            meal
+        )
+
+    }
+
+}
